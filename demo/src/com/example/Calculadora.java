d82fbc101694085a7678f0562e0067b45707a0e3
Javier <jmartin@grupoloyal.com>
2024-11-14 17:36:16 +0100

    Demo test

--- demo/src/com/example/Calculadora.java
+++ demo/src/com/example/Calculadora.java
@@ -1,8 +1,14 @@
 package com.example;
 
+import java.math.BigDecimal;
+
 public class Calculadora {
 	public double suma(double a, double b) {
 		return a + b;
+	}
+	
+	public BigDecimal suma(BigDecimal a, BigDecimal b) {
+		return a.add(b);
 	}
 	
 	public int suma(int a, int b) {
