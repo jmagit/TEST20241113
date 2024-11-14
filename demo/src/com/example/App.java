46a14c7f3eba922b2463653f86a7b4895559da17
Javier <jmartin@grupoloyal.com>
2024-11-14 16:54:37 +0100

    proyecto

---
+++ demo/src/com/example/App.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class App {
+
+	public static void main(String[] args) {
+		var calc = new Calculadora();
+		
+		//System.out.println("Suma enteros: " + calc.suma(2, 2));
+		//System.out.println("Suma reales: " + calc.suma(2.0, 1.5));
+
+	}
+
+}
---
+++ demo/src/com/example/Calculadora.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Calculadora {
+	public double suma(double a, double b) {
+		return a + b;
+	}
+	
+	public int suma(int a, int b) {
+		return a + b;
+	}
+}
