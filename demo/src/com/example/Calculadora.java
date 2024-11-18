c1d4a54b40c313126aed0f0154f6bd1eb9ceece9
Javier <jmartin@grupoloyal.com>
2024-11-18 16:01:11 +0100

    RoundIEEE754

--- demo/src/com/example/Calculadora.java
+++ demo/src/com/example/Calculadora.java
@@ -1,10 +1,17 @@
 package com.example;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Calculadora {
+	private double RoundIEEE754(double o) {
+		return (new BigDecimal(o))
+				.setScale(16, RoundingMode.HALF_UP)
+				.doubleValue();
+	}
+
 	public double suma(double a, double b) {
-		return a + b;
+		return RoundIEEE754(a + b);
 	}
 	
 	public BigDecimal suma(BigDecimal a, BigDecimal b) {
