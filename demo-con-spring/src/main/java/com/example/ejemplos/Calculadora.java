3333c4c8acfa7fd999b026aa3dddbe645532922e
Javier <jmartin@grupoloyal.com>
2024-11-18 16:47:17 +0100

    proyecto spring

---
+++ demo-con-spring/src/main/java/com/example/ejemplos/Calculadora.java
@@ -0,0 +1,24 @@
+package com.example.ejemplos;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Calculadora {
+	private double RoundIEEE754(double o) {
+		return (new BigDecimal(o))
+				.setScale(16, RoundingMode.HALF_UP)
+				.doubleValue();
+	}
+
+	public double suma(double a, double b) {
+		return RoundIEEE754(a + b);
+	}
+	
+	public BigDecimal suma(BigDecimal a, BigDecimal b) {
+		return a.add(b);
+	}
+	
+	public int suma(int a, int b) {
+		return a + b;
+	}
+}
---
+++ demo-con-spring/src/test/java/com/example/ejemplos/CalculadoraTest.java
@@ -0,0 +1,75 @@
+package com.example.ejemplos;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CalculadoraTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testSumaDoubleDoubleSinDecimales() {
+		var calculadora = new Calculadora();
+		
+		var actual = calculadora.suma(1.0, 2.0);
+		
+		assertEquals(3, actual);
+	}
+
+	@Test
+	void testSumaDoubleDoubleConDecimales() {
+		var calculadora = new Calculadora();
+		
+		var actual = calculadora.suma(0.1, 0.2);
+		
+		assertEquals(0.1, calculadora.suma(1, -0.9));
+		assertEquals(0.3, actual);
+	}
+
+	@Test
+	void testSumaIntInt() {
+		var calculadora = new Calculadora();
+		
+		var actual = calculadora.suma(2, 2);
+		
+		assertEquals(4, actual);
+	}
+	@Test
+	void testSumaIntIntMax() {
+		var calculadora = new Calculadora();
+		
+		var actual = calculadora.suma(Integer.MAX_VALUE, 1);
+		
+		assertEquals(Integer.MIN_VALUE, actual);
+	}
+
+	@Test
+	void testSumaBigDecimal() {
+		var calculadora = new Calculadora();
+		
+		var actual = calculadora.suma(BigDecimal.valueOf(0.1), BigDecimal.valueOf(0.2));
+		
+		assertEquals(BigDecimal.valueOf(0.3), actual);
+	}
+}
