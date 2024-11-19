5e039556a0e35d61fe5ec5f42c8416c6f20e5579
Javier <jmartin@grupoloyal.com>
2024-11-19 16:52:26 +0100

    assert

--- demo-con-spring/src/main/java/com/example/ejemplos/Persona.java
+++ demo-con-spring/src/main/java/com/example/ejemplos/Persona.java
@@ -1,15 +1,18 @@
 package com.example.ejemplos;
 
+import java.util.Optional;
+
 public class Persona {
 	private int id;
 	private String nombre;
 	private String apellidos;
 	
+	private Persona() { }
 	
 	public Persona(int id, String nombre, String apellidos) {
 		this.id = id;
 		setNombre(nombre);
-		this.apellidos = apellidos;
+		setApellidos(apellidos);
 	}
 	
 	public int getId() {
@@ -22,17 +25,34 @@ public class Persona {
 		return nombre;
 	}
 	public void setNombre(String nombre) {
+		if(nombre == null)
+			throw new IllegalArgumentException("El nombre no puede ser nulo.");
 		this.nombre = nombre;
 	}
-	public String getApellidos() {
-		return apellidos;
+	public Optional<String> getApellidos() {
+		return Optional.ofNullable(apellidos);
 	}
 	public void setApellidos(String apellidos) {
+		if(apellidos == null)
+			throw new IllegalArgumentException("Los apellidos no pueden ser nulo.");
 		this.apellidos = apellidos;
 	}
+	
 	@Override
 	public String toString() {
 		return "Persona [id=" + id + ", nombre=" + nombre + ", apellidos=" + apellidos + "]";
 	}
-	
+
+	public static Persona creaPersona(String nombre, String apellidos) {
+//		return null;
+//		throw new ArithmeticException();
+		return new Persona(0, nombre, apellidos);
+	}
+
+	public static Persona creaPersona(String nombre) {
+		var p = new Persona();
+		p.setNombre(nombre);
+		return p;
+	}
+
 }
---
+++ demo-con-spring/src/test/java/com/example/ejemplos/PersonaTest.java
@@ -0,0 +1,47 @@
+package com.example.ejemplos;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+class PersonaTest {
+	@Nested
+	class OK {
+
+		@Test
+		void createPersona() {
+			var p = Persona.creaPersona("Pepito", "Grillo");
+
+			assertNotNull(p);
+			assertAll("Propiedade", 
+					() -> assertEquals(0, p.getId()),
+					() -> assertEquals("Pepito", p.getNombre(), "Nombre"),
+					() -> assertEquals("Grillo", p.getApellidos().get(), "Apellidos"));
+		}
+		@Test
+		void createPersonaSinApellidos() {
+			var p = Persona.creaPersona("Pepito");
+
+			assertNotNull(p);
+			assertAll("Propiedade", 
+					() -> assertEquals(0, p.getId()),
+					() -> assertEquals("Pepito", p.getNombre(), "Nombre"),
+					() -> assertTrue(p.getApellidos().isEmpty(), "Apellidos"));
+		}
+	}
+
+	@Nested
+	class KO {
+		@Test
+		void createPersonaSinNombre() {
+			var ex = assertThrows(IllegalArgumentException.class, () -> Persona.creaPersona(null, "Grillo"));
+//			var ex = assertThrows(IllegalArgumentException.class, () -> Persona.creaPersona("Grillo", null));
+			assertEquals("El nombre no puede ser nulo.", ex.getMessage());
+		}
+		@Test
+		void createPersonaSinApellidos() {
+			assertThrows(IllegalArgumentException.class, () -> Persona.creaPersona("Grillo", null ));
+		}
+	}
+}
